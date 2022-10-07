d4156a9db00478b4464d0e78f20cacf2dd67caf4
nayabjahan <nayabzafir@gmail.com>
2022-10-08 01:09:39 +0530

    Added:Functionality to check whether year is leap or not

---
+++ LeapYear.js
@@ -0,0 +1,16 @@
+function check_leapyear(){
+    //define variables
+    var year;
+
+    year = prompt("enter the year to check");
+
+    //three conditions to find out the leap year
+    if( (0 == year % 4) && (0 != year % 100) || (0 == year % 400) )
+    {
+        console.log(year+" is a leap year");  
+    }
+    else
+    {
+    console.log(year+" is not a leap year");  
+    }
+}
